6831492075b42c949ca06e42ee70c6de9666d0aa
Rfallon89 <rory_fallon@hotmail.co.uk>
2023-02-06 15:44:27 +0000

    styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,8 @@ import {useEffect, useState} from 'react'
 import { getArticles} from '../utils/API'
 import { useParams} from "react-router-dom";
 import {Link} from 'react-router-dom'
+import create from '../icons/create-list.png'
+import '../styles/Home.css'
 
 
 export const Home = () => {
@@ -23,10 +25,14 @@ export const Home = () => {
                 return(
                     <div key={article.article_id} className='article'>
                         <Link to={`/articles/${article.article_id}`}><img id='article_img' src={article.article_img_url} alt={article.title}/></Link>
-                        <Link to={`/articles/${article.article_id}`}>{article.title}</Link>
+                        <div className='overview'>
+                        <Link id = 'title' to={`/articles/${article.article_id}`}><b>{article.title}</b></Link>
                         <p>{article.author}</p>
-                        <p>{article.created_at}</p>
-                        <Link to={`/topic/${article.topic}`}>{article.topic}</Link>
+                        <div className='created'>
+                        <p><img src= {create}/>{article.created_at.slice(0,10)}</p>
+                        <Link id='topic' to={`/topic/${article.topic}`}><b>{article.topic}</b></Link>
+                        </div>
+                        </div>
                     </div>
                 )
             })}
