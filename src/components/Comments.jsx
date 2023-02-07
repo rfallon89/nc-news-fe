35738549f1e01b8826ec81a1d8a7a8cf7d09c5f9
Rfallon89 <rory_fallon@hotmail.co.uk>
2023-02-07 15:48:36 +0000

    removed add comment

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,28 +1,20 @@
-import { useEffect, useState, useContext } from "react"
-import { getCommentsbyArticleId ,postComment} from "../utils/API"
-import { UserContext } from "../context/user"
+import { useEffect, useState } from "react"
+import { getCommentsbyArticleId } from "../utils/API"
 import { CommentList } from "./CommentList"
 
 export const Comments = ({article_id}) =>{
     const [comments,setComments] = useState([])
     const [viewComments, setViewComments] = useState(false)
-    const {user:username} = useContext(UserContext)
+   
     
     useEffect(()=>{
         getCommentsbyArticleId(article_id).then(comments=>setComments(comments))
     })
 
-    const addComment = (e) =>{
-        e.preventDefault()
-        postComment(article_id,username,e.target[0].value).then(comment=>setComments(...comments,comment))
-    }
-
+  
     return(
         <section>
-            <form onSubmit = {addComment}>
-                <textarea name="addComment" placeholder='Add new comment...'cols='70' rows="10" charswidth='23'></textarea>
-                <button>Add Comment</button>
-            </form>
+           
 
             <button onClick={()=>setViewComments(!viewComments)}>View Comments</button>
             
