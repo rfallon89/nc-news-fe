55e70cfc505ff2cad3d9006d93911096c6fab191
Rfallon89 <rory_fallon@hotmail.co.uk>
2023-02-08 12:48:57 +0000

    delete comment functionality

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 import messageIcon from '../icons/message.png'
 import like from '../icons/thumb-up.png'
 import dislike from '../icons/dislike.png'
 import '../styles/CommentList.css'
-export const CommentList = ({comments,comment_count}) =>{
+import { DeleteComment } from "./DeleteComment"
+import {UserContext} from '../context/user'
 
+export const CommentList = ({comments,setComments,article:{comment_count,author}}) =>{
+    const {user:{username}} = useContext(UserContext)
     const [viewComments, setViewComments] = useState(false)
     
     return  (
@@ -16,17 +19,23 @@ export const CommentList = ({comments,comment_count}) =>{
             
                 {viewComments
                 ?<ul id='commentContainer'>
-                    {comments.map(comment=>{
+                    {comments.map((comment,index)=>{
                         return (
                             <li id='commentLayout' key={comment.comment_id}>
+                                {comment.author?
+                                <>
                                 <div id='post'>
-                                    <p>{comment.author}</p>
+                                    <p>{comment.author} -</p>
                                     <p>{new Date(comment.created_at).toDateString()}</p>
                                 </div>
                          
                                 <p>{comment.body}</p>
-
+                                <div>
                                 <div id='likes'><img src={like} alt='like icon'/><p>{comment.votes}</p><img src={dislike} alt='dislike icon'/></div>
+                                {username===author || username===comment.author?<DeleteComment commentPosition = {index} setComments ={setComments} commentId = {comment.comment_id}/>:null}
+                                </div>
+                                </>
+                                :  <p>Comment ID: {comment.comment_id} has been removed</p> }
                             </li>
                             )
                     })}
