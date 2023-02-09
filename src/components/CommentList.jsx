f028e180b28ea09b7ab2df32b105d7264ffe4004
Rfallon89 <rory_fallon@hotmail.co.uk>
2023-02-09 11:48:42 +0000

    comment list refactored

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -6,7 +6,7 @@ import '../styles/CommentList.css'
 import { DeleteComment } from "./DeleteComment"
 import {UserContext} from '../context/user'
 
-export const CommentList = ({comments,setComments,article:{comment_count,author}}) =>{
+export const CommentList = ({comments,setComments,article}) =>{
     const {user:{username}} = useContext(UserContext)
     const [viewComments, setViewComments] = useState(false)
     
@@ -14,28 +14,28 @@ export const CommentList = ({comments,setComments,article:{comment_count,author}
             <>
                 <div id='commentsList'>
                     <button id='comments_toggle' onClick={()=>setViewComments(!viewComments)}><img src={messageIcon} alt='message icon'/> Comments</button>
-                <p>{comment_count}</p>
+                <p>{article.comment_count}</p>
                 </div>
             
                 {viewComments
                 ?<ul id='commentContainer'>
-                    {comments.map((comment,index)=>{
+                    {comments.map(({comment_id,author,created_at,body,votes},index)=>{
                         return (
-                            <li id='commentLayout' key={comment.comment_id}>
-                                {comment.author?
+                            <li id='commentLayout' key={comment_id}>
+                                {author?
                                 <>
                                 <div id='post'>
-                                    <p>{comment.author} -</p>
-                                    <p>{new Date(comment.created_at).toDateString()}</p>
+                                    <p>{author} -</p>
+                                    <p>{new Date(created_at).toDateString()}</p>
                                 </div>
                          
-                                <p>{comment.body}</p>
+                                <p>{body}</p>
                                 <div>
-                                <div id='likes'><img src={like} alt='like icon'/><p>{comment.votes}</p><img src={dislike} alt='dislike icon'/></div>
-                                {username===author || username===comment.author?<DeleteComment commentPosition = {index} setComments ={setComments} commentId = {comment.comment_id}/>:null}
+                                <div id='likes'><img src={like} alt='like icon'/><p>{votes}</p><img src={dislike} alt='dislike icon'/></div>
+                                {username===article.author || username===author?<DeleteComment commentPosition = {index} setComments ={setComments} commentId = {comment_id}/>:null}
                                 </div>
                                 </>
-                                :  <p>Comment ID: {comment.comment_id} has been removed</p> }
+                                :  <p>Comment ID: {comment_id} has been removed</p> }
                             </li>
                             )
                     })}
